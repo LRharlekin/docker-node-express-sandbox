f97678af7f0ae564e48a7dac5fa942f6df653a4a
LRharlekin <13718192+LRharlekin@users.noreply.github.com>
2024-01-07 22:30:09 +0200

    basic Dockerfile config

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("<h2>Hi there</h2>");
+});
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server listening on PORT:${port}`);
